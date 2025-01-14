06a86efc7637d41ce94717b8053c0b25796cb59c
trungnt173 <trungnt173@fpt.edu.vn>
2025-01-14 15:14:40 +0700

    day 4

--- Function.php
+++ Function.php
@@ -37,12 +37,37 @@ function tinhHieu(){
 function tinhTich($a, $b){
     echo $a * $b;
 }
-// Hàm không có trả không có tham số
+// Hàm không có trả về không có tham số
 //Công thức
 // function ten_ham(){
 //     // code
 // }
-// Lưu ý: Nếu hàm không có tham số thì sẽ không
+function tinhThuong(){
+    echo 6 / 3;
+}
+// Lưu ý: 
+// -Nếu hàm không có tham số thì sẽ không
 // Nhận được giá trị từ bên ngoài hàm truyền vào nên 
 // Phải tự tạo giá trị
+//- Hàm có bao nhiều tham số thì truyền vào bấy
+// Nhiêu giá trị
+// Gọi hàm: Khi sử dụng ta cần phải gọi hàm
+// Hàm có trả về có tham số
+// $tên_biến = tên_ham($thamso_1, $thamso_2, ..., $thamso_n);
+// Ví dụ
+$tinhTong = tinhTong(5, 6); // Có 2 tham số
+// Hàm có trả về không có tham số
+// Hàm có trả về có tham số
+// $tên_biến = tên_ham();
+$tinhHieu = tinhHieu();// Không có tham số
+// Hàm không có trả về có tham số
+// tên_hàm($thamso_1, $thamso_2, ..., $thamso_n);
+tinhTich(5,6); // có 2 tham số
+// Hàm không có trả về không có tham số
+// tên_hàm();
+tinhThuong();
+// Luyện tập hàm
+// Viết tính phương trình bậc 2 theo 4 loại hàm
+// Viết phương trình có trả về và có tham số. Đếm trong mảng
+// có bao nhiêu số chinh phương và số hoàn hảo và số nguyên tố 
 ?>
