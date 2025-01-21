8f190e68574bb1b20db81fe8c73697185ffee635
trungnt173 <trungnt173@fpt.edu.vn>
2025-01-21 15:05:07 +0700

    Day 7

---
+++ mvc/index.php
@@ -0,0 +1,17 @@
+<!-- ASM 1: Yêu cầu xây dựng giao diện cho những trang sau
+ - Admin (dạng bảng)
+ 1: Danh sản phẩm 
+ - Hiện thi
+ - Thêm
+ - Sửa
+ 2: Sản phẩm
+ - Người dùng
+ 1: Trang chủ
+ Có thể tham khảo hoặc sự các theme của bootstrap
+ Nhưng cần tách thành 2 file 1 là layout 2 nội dung
+ -> ASM 1 lấy: Lab 1234 (Có điểm cộng) và ASM1
+-->
+ <?php 
+$view = 'views/admin/category/add.php';
+include 'views/admin/layout.php';
+ ?>
---
+++ mvc/views/admin/layout.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <H1>Xin Chào</H1>
+    <?php 
+      if(isset($view)){
+        include $view;
+      }
+    ?>
+</body>
+</html>
